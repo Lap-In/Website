dfbbd247cd3b63a0de196f90834cfa9c1f8fc5e8
Lapin <lapin@Muffin-PC.muffin.pon>
2016-11-30 14:27:52 +0100

    Current state of website

---
+++ http/api/index.php
@@ -0,0 +1,24 @@
+<?php
+// get the HTTP method// get the HTTP method, path and body of the request
+$method = $_SERVER['REQUEST_METHOD'];
+$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+$input = json_decode(file_get_contents('api/model.json'),true);
+ 
+ 
+// create SQL based on HTTP method
+if ($method == 'GET') {
+	echo "" . $request;
+}
+ 
+// print results, insert id or affected row count
+/*if ($method == 'GET') {
+  if (!$key) echo '[';
+  for ($i=0;$i<mysqli_num_rows($result);$i++) {
+    echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
+  }
+  if (!$key) echo ']';
+} elseif ($method == 'POST') {
+  echo mysqli_insert_id($link);
+} else {
+  echo mysqli_affected_rows($link);
+}*/
